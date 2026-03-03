c164db3f94f290e604b1f3154f5cd708e05fa941
20148847 <20148847@tafe.wa.edu.au>
2026-03-03 15:40:05 +0800

    first commit

---
+++ NapsterMobileDevelopment/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NapsterMobileDevelopment
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NapsterMobileDevelopment/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NapsterMobileDevelopment
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
